550da4a71aa71d007081a069f70c61d2f34532c3
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2023-11-01 22:11:21 +0200

    generics task 4 done

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,11 +1,12 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
+interface PropsType {
+  title: string;
+};
 
 class Component {
-  constructor (public props:T) {
-
-  }
+  constructor (public props: PropsType) {}
 }
 
 class Page extends Component {
